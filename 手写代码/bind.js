c3e1f3c603402e5a8a53e93442764b3073e9cd93
liangyancheng <656040874@qq.com>
2024-05-27 16:49:33 +0800

    手写bind

--- 手写代码/bind.js
+++ 手写代码/bind.js
@@ -4,7 +4,8 @@
 Function.prototype._bind = function (ctx, ...args) {
   const _self = this; // this 指向的是调用bind的对象，即是需要执行的函数
   const newFun = function (...args2) {
-    return _self.call(ctx, ...args, ...args2);
+    // return _self.call(ctx, ...args, ...args2);
+    return _self.call(this instanceof newFun ? this : ctx, ...args, ...args2); // 当bind返回的函数使用new构建实例的时候，ctx为new出来的实例对象
   };
   if (_self.prototype) {
     newFun.prototype = Object.create(_self.prototype);
